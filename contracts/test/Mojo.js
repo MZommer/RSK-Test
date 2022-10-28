999ae7e78642fd1b26c8d804daa0d6a47b9aa996
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2022-10-28 12:27:50 -0300

    Update Mojo.js

--- contracts/test/Mojo.js
+++ contracts/test/Mojo.js
@@ -1,5 +1,6 @@
 const hre = require("hardhat");
 const { assert } = require("chai");
+const { BigNumber } = require("ethers");
 
 async function deploy() {
     const Mojo = await hre.ethers.getContractFactory("Mojo");
@@ -13,23 +14,24 @@ describe("Mojo", () => {
     const mojo = await deploy();
     // checks msg.sender balance is 100
     const [owner] = await hre.ethers.getSigners();
-    console.log(await mojo.balanceOf(owner))
-    assert.equal(await mojo.balanceOf(owner), 100);
+    assert.equal(await mojo.balanceOf(owner.address), 100);
   });
   it("Make transactions", async () => {
     // random addresses
     const tx = [{
         address: "0x29D7d1dd5B6f9C864d9db560D72a247c178aE86B",
-        amount: 10,
+        amount: BigNumber.from("10"),
     },{
         address: "0xf07ba2229b4da47895ce0a4ab4298ad7f8cb3a4d",
-        amount: 20,
+        amount: BigNumber.from("20"),
     },{
         address: "0x29d7d1dd5b6f9c864d9db560d72a247c178ae86b",
-        amount: 30,
+        amount: BigNumber.from("30"),
     }];
 
     const mojo = await deploy();
+
+    console.log("aaaa")
     
     for(const {address, ammount} of tx)  // maybe a better aproach could be with Promises.all([transactions])
         await mojo.transfer(address, ammount);
