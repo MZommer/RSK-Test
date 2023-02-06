04025eb15b06c1d478f653752415b0d8250f552b
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2023-02-05 22:12:33 -0300

    Update helpers.js

--- contracts/utils/helpers.js
+++ contracts/utils/helpers.js
@@ -1,25 +1,26 @@
 const hre = require("hardhat");
 
-module.deployMojo = async () => {
+module.exports = {
+  async deployMojo() {
     const Mojo = await hre.ethers.getContractFactory("Mojo");
     const mojo = await Mojo.deploy();
     await mojo.deployed();
     return mojo;
-}
-module.deployGacha = async (mojoAddress) => {
-  const Gacha = await hre.ethers.getContractFactory("Gacha");
-  const gacha = await Gacha.deploy(mojoAddress);
-  await gacha.deployed();
-  return gacha;
-}
-module.deployAvatar =  async (gachaAddress) => {
-  const Avatar = await hre.ethers.getContractFactory("Avatar");
-  const avatar = await Avatar.deploy(gachaAddress);
-  await avatar.deployed();
-  return avatar;
-}
+  },
+  async deployGacha(mojoAddress) {
+    const Gacha = await hre.ethers.getContractFactory("Gacha");
+    const gacha = await Gacha.deploy(mojoAddress);
+    await gacha.deployed();
+    return gacha;
+  },
+  async deployAvatar(gachaAddress) {
+    const Avatar = await hre.ethers.getContractFactory("Avatar");
+    const avatar = await Avatar.deploy(gachaAddress);
+    await avatar.deployed();
+    return avatar;
+  },
 
-module.skipBlocks = async (nbBlocks) => {
-  await Promise.all(Array(nbBlocks).map(() => ethers.provider.send("evm_mine", [1000])))
-}
-
+  async skipBlocks(nbBlocks) {
+    await Promise.all(Array(nbBlocks).map(() => ethers.provider.send("evm_mine", [1000])))
+  },
+};
